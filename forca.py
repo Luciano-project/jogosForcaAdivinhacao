91fd280a35f73cf6ed263a335ad250a6d17fb863
Luciano-project <luhugo.lhcs@gmail.com>
2022-06-27 15:53:22 +0100

    Adicionado mensagens aos usuários

--- forca.py
+++ forca.py
@@ -30,13 +30,14 @@ def jogando(palavra_secreta,letras_acertadas):
             marca_chute_correto(chute,palavra_secreta,letras_acertadas)
         else:
             erros += 1
-            print("Você errou, tente novamente!\nTentativas testantes {}".format(6-erros))
+            desenha_forca(erros)
 	
-        enforcou = erros == 6
+        enforcou = erros == 7
         acertou = "_" not in letras_acertadas
         print("\n{}".format(letras_acertadas))
     return acertou
-     
+
+
 def jogar():
 
     cabecalho()    
@@ -49,12 +50,100 @@ def jogar():
     print(letras_acertadas)
     
     resultado=jogando(palavra_secreta,letras_acertadas)      
-        
-        	
+            	
     print("Fim do jogo")
     
-    if resultado: print("você ganhou!")
-    else: print("Você perdeu!\n\nA palavra era: {}".format(palavra_secreta))
+    if resultado:
+        imprime_mensagem_vencedor()
+        
+    else:
+        imprime_mensagem_perdedor(palavra_secreta)
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+def imprime_mensagem_perdedor(palavra_secreta):
+
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
 
 if(__name__ == "__main__"):
     jogar()
